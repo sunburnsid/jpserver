3cb984f98791bdab4a83262b33cfaa7e98bd7585
sunburnsid <sb846@cornell.edu>
2016-11-10 15:47:39 -0500

    changes to views

--- app/views.py
+++ app/views.py
@@ -1,9 +1,3 @@
-# -*- encoding: utf-8 -*-
-"""
-Python Aplication Template
-Licence: GPLv3
-"""
-
 from flask import url_for, redirect, render_template, flash, g, session
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, lm
